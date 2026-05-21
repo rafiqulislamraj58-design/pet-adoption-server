3d94f470582f04a3c669f15ba0b1ab5b9d8ece76
rafiqulislamraj58-design <rafiqulislamraj58@gmail.com>
2026-05-21 10:20:57 +0600

    update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+const petRoutes = require('./routes/petRoutes');
+const requestRoutes = require('./routes/requestRoutes');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors({
@@ -14,9 +19,13 @@ app.use(cookieParser());
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB Connected!'))
   .catch((err) => console.log(err));
+app.use('/pets', petRoutes);
+app.use('/requests', requestRoutes);
+app.use('/auth', authRoutes);
 app.get('/', (req, res) => {
   res.send('Pet Adoption Server Running!');
 });
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
